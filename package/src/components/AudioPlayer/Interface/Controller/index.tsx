fb607d58bc2b189aabed4c2611779ca6ac61585b
goradels <gadelson1126@gmail.com>
2022-12-24 14:59:13 -0500

    remove next prev buttons

--- package/src/components/AudioPlayer/Interface/Controller/index.tsx
+++ package/src/components/AudioPlayer/Interface/Controller/index.tsx
@@ -4,7 +4,7 @@ import {
   audioPlayerStateContext,
   defaultInterfacePlacement,
 } from "@/components/AudioPlayer/Context/StateContext";
-import { PlayBtn, PrevNnextBtn, RepeatTypeBtn } from "./Button";
+import { PlayBtn, RepeatTypeBtn } from "./Button";
 import { SortablePlayList } from "./Drawer";
 import { Progress } from "./Input";
 import { Flex } from "@react-spectrum/layout";
@@ -54,15 +54,7 @@ export const Controller: FC = () => {
         visible={Boolean(activeUI.playButton ?? activeUI.all)}
       >
         <Flex UNSAFE_className="btn-wrapper" alignItems={"center"} gap={"10px"}>
-          <PrevNnextBtn
-            type="prev"
-            visible={Boolean(activeUI.prevNnext ?? activeUI.all)}
-          />
           <PlayBtn />
-          <PrevNnextBtn
-            type="next"
-            visible={Boolean(activeUI.prevNnext ?? activeUI.all)}
-          />
         </Flex>
       </Grid.Item>
       <Grid.Item
